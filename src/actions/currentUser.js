cedf8c49409bc98c45a1dd3cf672a57e19b0c66e
Marc Mills <mdmills1990@gmail.com>
2019-07-11 13:32:23 -0600

    adds credentials: include for the action creators login and getcurrentuser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,18 +1,49 @@
 export const setCurrentUser = user => {
   return {
     type: "SET_CURRENT_USER",
-    payload: user
+    user
   }
 }
 
-export const login = credentials => {
-    return dispatch => {
-      return fetch("http://localhost:3001/api/v1/login", {
+export const login = (credentials) => {
+  return(dispatch) => {
+      return fetch('http://localhost:3001/api/v1/login', {
+        credentials: "include",
         method: "POST",
         headers: {
           "Content-Type" : "application/json"
         },
         body: JSON.stringify(credentials)
       })
+        .then(r => r.json())
+        .then(user => {
+          if (user.errors){
+            alert(user.errors)
+          } else {
+            dispatch(setCurrentUser(user))
+          }
+        })
+        .catch(console.log)
+  }
+}
+
+export const getCurrentUser = () => {
+    return dispatch => {
+      return fetch("http://localhost:3001/api/v1/getCurrentUser", {
+        credientials: 'include',
+        method: "GET",
+        headers: {
+          "Content-Type" : "application/json"
+        }
+      })
+        .then(r => r.json())
+        .then(user => {
+          if (user.errors){
+            alert(user.errors)
+          } else {
+            dispatch(setCurrentUser(user))
+          }
+        })
+        .catch(console.log)
   }
 }
