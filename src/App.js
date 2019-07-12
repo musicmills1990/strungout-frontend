24c090471578788e1aac7c199344851ed7bdeb78
Marc Mills <mdmills1990@gmail.com>
2019-07-12 11:22:39 -0600

    added logout functionality, adds statetoprops to do this

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Login from "./components/Login.js"
+import Logout from "./components/Logout.js"
 import { connect } from "react-redux"
 import { getCurrentUser } from "./actions/currentUser.js"
 class App extends React.Component {
@@ -8,13 +9,21 @@ class App extends React.Component {
   componentDidMount(){
     this.props.getCurrentUser()
   }
+
   render(){
     return (
       <div>
         <h1>Welcome To StrungOut</h1>
-        <Login/>
+        {this.props.currentUser ? <Logout/> : <Login/>}
       </div>
     );
   }
 }
-export default connect(null, { getCurrentUser })(App);
+
+const mapStateToProps = ({ currentUser}) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
