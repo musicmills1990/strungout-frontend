b6ad44c5332c3b2796b20594e96319d84211a985
Marc Mills <mdmills1990@gmail.com>
2019-07-12 12:16:58 -0600

    stubbed out to start to add the mainpage stuff here

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const MainContainer = () => {
+  return (
+    <div class="MainContainer">
+    <p>hey its the main container!</p>
+    </div>
+  )
+}
+
+export default MainContainer
