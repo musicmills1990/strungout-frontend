41493b9229b2b8dd21ca5fc7c14f6c7acddfebc8
Marc Mills <mdmills1990@gmail.com>
2019-07-12 18:55:07 -0600

    adds the myGuitar reducer

--- src/store.js
+++ src/store.js
@@ -3,11 +3,14 @@ import thunk from 'redux-thunk'
 import users from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
+import myGuitars from './reducers/myGuitars.js'
+
 
 const reducer = combineReducers({
   users,
   currentUser,
-  loginForm
+  loginForm,
+  myGuitars
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
