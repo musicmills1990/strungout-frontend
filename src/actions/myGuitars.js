7e56ec82e1970da75aee1da72a6fe25befe3c60c
Marc Mills <mdmills1990@gmail.com>
2019-07-12 18:52:56 -0600

    made a new fetch request to populate the myGuitar store in redux

---
+++ src/actions/myGuitars.js
@@ -0,0 +1,28 @@
+export const setMyGuitars = guitars => {
+  return {
+    type: "SET_MY_GUITARS",
+    guitars
+  }
+}
+
+
+export const getMyGuitars = () => {
+  return dispatch => {
+  fetch("http://localhost:3001/api/v1/guitars",{
+    credentials: "include",
+    method: "GET",
+    headers: {
+      "Content-Type": "appication/json"
+    }
+  })
+    .then(r=> r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        dispatch(setMyGuitars(response.data))
+      }
+    })
+    .catch(console.log)
+  }
+}
