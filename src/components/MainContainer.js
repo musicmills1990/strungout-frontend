81dfb9cfade21fffe627bfe45535fec9e3197cb7
Marc Mills <mdmills1990@gmail.com>
2019-07-12 18:51:15 -0600

    adds myguitar component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import MyGuitars from "./MyGuitars.js"
 
 const MainContainer = () => {
+  
   return (
-    <div class="MainContainer">
+    <div className="MainContainer">
     <p>hey its the main container!</p>
+    <MyGuitars/>
     </div>
   )
 }
