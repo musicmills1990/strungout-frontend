42f9104c75a99a79426d68c68f7950f33db42598
Marc Mills <mdmills1990@gmail.com>
2019-07-16 09:54:46 -0600

    builds out stringPack componenet to deliver stringpack data to frontend

---
+++ src/components/MyStringPacks.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import StringPackCard from './StringPackCard.js'
+
+const MyStringPacks = (props) => {
+  console.log(props)
+  const stringPackCard = props.stringPacks.map(str => <StringPackCard stringPack={str} key={str.id}/>)
+  return (
+    stringPackCard.length > 0 ? stringPackCard : null
+  )
+}
+
+
+const mapStateToProps = state => {
+  return {
+    stringPacks: state.stringPacks
+  }
+}
+
+export default connect(mapStateToProps)(MyStringPacks)
