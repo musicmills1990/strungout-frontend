be7416bfbce57257ab02bf9843ce4f2c04ce665a
Marc Mills <mdmills1990@gmail.com>
2019-07-17 15:20:50 -0600

    added newGuitar post request

--- src/actions/myGuitars.js
+++ src/actions/myGuitars.js
@@ -26,3 +26,24 @@ export const getMyGuitars = () => {
     .catch(console.log)
   }
 }
+
+export const newGuitar = () => {
+  return dispatch => {
+  fetch("http://localhost:3001/api/v1/guitars",{
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "appication/json"
+    }
+  })
+    .then(r=> r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        dispatch(setMyGuitars(response.data))
+      }
+    })
+    .catch(console.log)
+  }
+}
