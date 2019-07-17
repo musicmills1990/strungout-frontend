7b5bdc3fe14de59bd005a087dd7f0fb5aedc7b52
Marc Mills <mdmills1990@gmail.com>
2019-07-17 15:23:40 -0600

    following redux pattern to grab data for the new guitar to send in post request

---
+++ src/reducers/newGuitarForm.js
@@ -0,0 +1,18 @@
+const initialState = {
+  guitar_type: "",
+  brand: "",
+  model: "",
+  color: "",
+  user_id: ""
+}
+
+export default (state = initialState, action) => {
+  switch(action.type){
+    case "UPDATE_GUITAR_FORM":
+      return action.formData
+    case "RESET_GUITAR_FORM":
+      return initialState
+    default:
+      return state
+  }
+}
