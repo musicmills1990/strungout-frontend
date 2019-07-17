669ea194ce9cf13867f3a60790c0c0d3597a6182
Marc Mills <mdmills1990@gmail.com>
2019-07-17 15:21:19 -0600

    adds form to turnerylogic

--- src/components/MyGuitars.js
+++ src/components/MyGuitars.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import GuitarCard from './GuitarCard.js'
+import NewGuitar from './NewGuitar'
 
 const MyGuitars = (props) => {
 
   const guitarCard = props.guitars.map(g => <GuitarCard guitar={g} key={g.id}/>)
   return (
-    guitarCard.length > 0 ? guitarCard : "Add a new guitar!"
+    guitarCard.length > 0 ? guitarCard : <NewGuitar/>
   )
 }
 
