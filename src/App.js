5445e0a1f9f044a347a32c26635a5b9490c98e3c
Marc Mills <mdmills1990@gmail.com>
2019-07-17 18:24:00 -0600

    deletes MainContainer for now..

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { connect } from "react-redux"
 import { getCurrentUser } from "./actions/currentUser.js"
 import NavBar from './components/NavBar.js'
-import MainContainer from './components/MainContainer.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from "./components/Login.js"
 import Signup from './components/Signup.js'
@@ -22,7 +21,6 @@ class App extends React.Component {
       <Router>
         <div>
         <NavBar/>
-        <MainContainer/>
         <Route exact path='/login' component={Login}/>
         <Route exact path='/signup' component={Signup}/>
         <Route exact path='/guitars' component={MyGuitars}/>
