d172196842f53d8d04dd3d7914ae2bb2ec61e42f
Marc Mills <mdmills1990@gmail.com>
2019-07-18 15:03:42 -0600

    now connects to store to grab currentUser so the user's address can be displayed

--- src/components/BrokenStringMessage.js
+++ src/components/BrokenStringMessage.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import BrokenStringMessageGraphic from '../brokenStringMessageGraphic.png'
+import { connect } from 'react-redux'
 
 
-const BrokenStringMessage = () => {
+const BrokenStringMessage = ({ currentUser }) => {
   return (
     <div className="brokenStringMessage">
-    <h3>Ach! Sorry your string broke, but we've got you covered!</h3>
-    <img src={BrokenStringMessageGraphic} alt="Sorry your string broke!" width="600" height="240"/>
-    <p>We're sending a new
-    ~stringpack.chosenString~ string to:
-
-     ~current_user.address~
-
-     and it should arrive in 2 business days! In the meantime,
+      <h3>Ach! Sorry your string broke, but we've got you covered!</h3>
+      <img src={BrokenStringMessageGraphic} alt="Sorry your string broke!" width="600" height="240"/>
+      <p>We're sending a new string to:</p>
+      <h2>{currentUser ? currentUser.attributes.address : null}</h2>
+      <p> and it should arrive in 2 business days! In the meantime,
      use one of your two backup strings and keep rockin!"</p>
      </div>
   )
 }
 
-export default BrokenStringMessage
+
+const mapStateToProps = ({ currentUser}) => {
+  return {
+    currentUser
+  }
+}
+
+
+export default connect(mapStateToProps)(BrokenStringMessage)
