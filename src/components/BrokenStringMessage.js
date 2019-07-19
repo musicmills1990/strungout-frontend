56c3fd40e3d0f76fc58effca287a1fd5adb8f524
Marc Mills <mdmills1990@gmail.com>
2019-07-19 15:12:36 -0600

    adds multiple mapStateToProps from redux store so the message has access to Myring counter infromation, also builds a basic function to return which string broke in the message

--- src/components/BrokenStringMessage.js
+++ src/components/BrokenStringMessage.js
@@ -3,12 +3,33 @@ import BrokenStringMessageGraphic from '../brokenStringMessageGraphic.png'
 import { connect } from 'react-redux'
 
 
-const BrokenStringMessage = ({ currentUser }) => {
+const BrokenStringMessage = ({ currentUser, myStringPacks }) => {
+
+//this function is purely placeholder to see if mulitple mapStateToProps would work. I really need a method that can assess which
+//string counter just increase from it's previous state, and should have anything to do with the values of the other string_counters
+  function stringName(){
+    if (myStringPacks[0].attributes.low_e_string_counter > myStringPacks[0].attributes.a_string_counter) {
+      return "Low E"
+    } else if (myStringPacks[0].attributes.a_string_counter > myStringPacks[0].attributes.d_string_counter) {
+      return "A"
+    } else if (myStringPacks[0].attributes.d_string_counter > myStringPacks[0].attributes.g_string_counter) {
+      return "D"
+    } else if (myStringPacks[0].attributes.g_string_counter > myStringPacks[0].attributes.b_string_counter) {
+      return "G"
+    } else if (myStringPacks[0].attributes.b_string_counter > myStringPacks[0].attributes.high_e_string_counter) {
+      return "B"
+    } else if (myStringPacks[0].attributes.high_e_string_counter > myStringPacks[0].attributes.low_e_string_counter) {
+      return "High E"
+    } else {
+      return "NO"
+    }
+  }
+
   return (
     <div className="brokenStringMessage">
       <h3>Ach! Sorry your string broke, but we've got you covered!</h3>
       <img src={BrokenStringMessageGraphic} alt="Sorry your string broke!" width="600" height="240"/>
-      <p>We're sending a new string to:</p>
+      <p>We're sending a new {stringName()} string to:</p>
       <h2>{currentUser ? currentUser.attributes.address : null}</h2>
       <p> and it should arrive in 2 business days! In the meantime,
      use one of your two backup strings and keep rockin!"</p>
@@ -17,11 +38,14 @@ const BrokenStringMessage = ({ currentUser }) => {
 }
 
 
-const mapStateToProps = ({ currentUser}) => {
+const mapStateToProps = state => {
   return {
-    currentUser
+    currentUser: state.currentUser,
+    myStringPacks: state.myStringPacks
   }
 }
+
+
 
 
 export default connect(mapStateToProps)(BrokenStringMessage)
