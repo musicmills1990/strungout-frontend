9a2a7d1f90b020ecd947fd26cc1f75e679bbb40a
Marc Mills <mdmills1990@gmail.com>
2019-07-19 15:14:12 -0600

    altered initial state of counter for experimentation (learned it needs to be changed on the backend)

--- src/reducers/myStringPacks.js
+++ src/reducers/myStringPacks.js
@@ -1,7 +1,17 @@
 const initialState = {
+  low_e_string_counter: 0,
+  a_string_counter: 0,
+  d_string_counter: 0,
+  g_string_counter: 1,
+  b_string_counter: 0,
+  high_e_string_counter: 0,
+  guage: "",
+  brand: "",
+  model: "",
+  price: ""
 }
 
-export default (state = [], action) => {
+export default (state = initialState, action) => {
   switch (action.type){
     case "SET_MY_STRINGPACKS":
     return action.stringPacks
