821438543e43958254c68b2c08d65af2d66535d4
Marc Mills <mdmills1990@gmail.com>
2019-07-23 15:32:57 -0600

    copied isCurrentUserEmpty function

--- src/components/BrokenStringMessage.js
+++ src/components/BrokenStringMessage.js
@@ -38,7 +38,7 @@ const BrokenStringMessage = ({ currentUser, myStringPacks }) => {
     <div className="brokenStringMessage">
       <h3>Ach! Sorry your string broke, but we've got you covered!</h3>
       <img src={BrokenStringMessageGraphic} alt="Sorry your string broke!" width="600" height="240"/>
-      <p>We're sending a new stringName() string to:</p>
+      <p>We're sending a new string to:</p>
       <h2>{!isCurrentUserEmpty(currentUser) ? currentUser.attributes.address : null}</h2>
       <p> and it should arrive in 2 business days! In the meantime,
      use one of your two backup strings and keep rockin!"</p>
