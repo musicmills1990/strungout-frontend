0e38947aaef2ea205a9183fdb394f43656a0c831
Marc Mills <mdmills1990@gmail.com>
2019-08-01 13:19:24 -0600

    now that data is sent in proper structure, needed to change the .chain of data call

--- src/components/GuitarCardLiveCode.js
+++ src/components/GuitarCardLiveCode.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const GuitarCardLiveCode = (props) => {
+  debugger
   return (
     <div>
-    <h3>My Guitar:</h3>
-    <i>{props.guitar_type}</i>
-    <p>Brand: {props.brand}</p>
-    <p>Model: {props.model}</p>
-    <p>Color: {props.color}</p>
+    <h3>Guitar</h3>
+    <i>{props.guitar.attributes.guitar_type}</i>
+    <p>Brand: {props.guitar.attributes.brand}</p>
+    <p>Model: {props.guitar.attributes.model}</p>
+    <p>Color: {props.guitar.attributes.color}</p>
     </div>
   )
 }
